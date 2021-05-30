e162c353910971346d0532abfd60aa189bc5c465
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2021-05-29 21:39:41 -0300

    Concluíndo Req 01

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const io = require('socket.io')(http, {
     methods: ['GET', 'POST'], // Métodos aceitos pela url
   },
 });
+const moment = require('moment');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -19,29 +20,24 @@ app.get('/', (req, res) => {
 
 let onlineUsers = [];
 io.on('connection', (socket) => {
-  console.log(`Conectado: ${socket.id}`);
   const newUserId = socket.id.slice(0, 16);
-  console.log(`newUserId: ${newUserId}`);
   onlineUsers.push(newUserId);
   socket.emit('whoAmI', newUserId);
   socket.emit('onlineUsers', onlineUsers);
   socket.broadcast.emit('onlineUsers', onlineUsers);
-  // socket.broadcast.emit('mensagemServer', { mensagem: `Iiiiiirraaaa! Fulano${fulano} acabou de se conectar :D` });
-  
-  /* socket.on('send-nickname', (socket) => {
-    console.log(socket);
-    users.push(socket);
-    console.log(users);
-  }); */
 
   socket.on('disconnect', () => {
-    console.log(`Desconectado: ${socket.id}`);
     onlineUsers = onlineUsers.filter((user) => user !== newUserId);
     socket.broadcast.emit('onlineUsers', onlineUsers);
   });
 
-  socket.on('message', (msg) => {
-    io.emit('messageServer', { message: msg });
+  socket.on('message', ({ chatMessage, nickname }) => {
+    io.emit('message', `${moment().format('DD-MM-YYYY HH:mm:ss')} - ${nickname}: ${chatMessage}`);
+  });
+
+  socket.on('setNickname', (nickName) => {
+    io.emit('whoAmI', nickName);
+    io.emit('onlineUsers', onlineUsers);
   });
 });
 
